a92b924a0b872ca06c16332a290042b500c4d0dc
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-10 13:36:16 -0500

    client details now stored in a database

---
+++ joust-backend-db-deployer/src/main/resources/db/migration/V20160710123800.001__insert_oauth_client_details.sql
@@ -0,0 +1,6 @@
+INSERT INTO oauth_client_details 
+  (client_id, client_secret, authorities, scope, authorized_grant_types) 
+VALUES 
+  ('nodejs',  'nodejs',  'ROLE_CLIENT', 'read,write,administer' , 'password,refresh_token,client_credentials'),
+  ('web-mvc', 'web-mvc', 'ROLE_CLIENT', 'read,write,administer' , 'password,refresh_token,client_credentials')
+;
