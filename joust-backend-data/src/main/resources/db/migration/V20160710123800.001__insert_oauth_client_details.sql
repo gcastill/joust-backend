3049e7d22f7cbe7fe7f79bc5738231739b282684
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-11 21:26:53 -0500

    fix sql issue

--- joust-backend-data/src/main/resources/db/migration/V20160710123800.001__insert_oauth_client_details.sql
+++ joust-backend-data/src/main/resources/db/migration/V20160710123800.001__insert_oauth_client_details.sql
@@ -1,5 +1,5 @@
 INSERT INTO oauth_client_details 
   (client_id, client_secret, authorities, scope, authorized_grant_types) 
 VALUES 
-  ('nodejs',  'nodejs',  'ROLE_CLIENT', 'read,write,administer' , 'password,refresh_token,client_credentials',
+  ('nodejs',  'nodejs',  'ROLE_CLIENT', 'read,write,administer' , 'password,refresh_token,client_credentials')
 ;
