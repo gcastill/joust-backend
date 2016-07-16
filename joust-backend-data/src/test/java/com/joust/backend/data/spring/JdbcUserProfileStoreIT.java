9ddcc233f54a18d913cc54b0ac1c43bfda65376e
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-16 14:05:04 -0500

    integration test using spring example

--- joust-backend-data/src/test/java/com/joust/backend/data/spring/JdbcUserProfileStoreIT.java
+++ joust-backend-data/src/test/java/com/joust/backend/data/spring/JdbcUserProfileStoreIT.java
@@ -1,12 +1,21 @@
 package com.joust.backend.data.spring;
 
-import org.junit.Test;
+import javax.annotation.Resource;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration("classpath:beans-data.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
 public class JdbcUserProfileStoreIT {
+
+  @Resource
+  JdbcUserProfileStore instance;
 
   @Test
   public void testBean() {
-
-    throw new IllegalAccessError();
+    System.out.println(instance.hashCode());
   }
 }
