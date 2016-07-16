d3e9d7101208cfa459c2f65104b60ba66132a05f
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-16 12:14:59 -0500

    added support for integration tests

---
+++ joust-backend-data/src/test/java/com/joust/backend/data/spring/JdbcUserProfileStoreIT.java
@@ -0,0 +1,17 @@
+package com.joust.backend.data.spring;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.joust.backend.test.meanbean.BeanTesterBuilder;
+
+public class JdbcUserProfileStoreIT {
+
+  @Test
+  public void testBean() {
+
+    throw new IllegalAccessError();
+  }
+}
