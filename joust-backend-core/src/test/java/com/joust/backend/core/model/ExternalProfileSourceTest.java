8ff848333b4d37275cf6cbce77955e13ad0181cf
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-19 06:50:04 -0500

    more unit tests

--- joust-backend-core/src/test/java/com/joust/backend/core/model/ExternalProfileSourceTest.java
+++ joust-backend-core/src/test/java/com/joust/backend/core/model/ExternalProfileSourceTest.java
@@ -3,6 +3,7 @@ package com.joust.backend.core.model;
 import java.util.UUID;
 
 import org.junit.Test;
+import org.meanbean.test.EqualsMethodTester;
 import org.meanbean.test.HashCodeMethodTester;
 import org.meanbean.util.RandomValueGenerator;
 import org.meanbean.util.SimpleRandomValueGenerator;
@@ -15,10 +16,14 @@ public class ExternalProfileSourceTest {
 
   @Test
   public void testHashCode() {
-    ExternalProfileSource eps = ExternalProfileSource.builder().referenceId(new String(generator.nextBytes(2)))
+    ExternalProfileSource source = ExternalProfileSource.builder().referenceId(new String(generator.nextBytes(2)))
         .source(generator.nextBoolean() ? Source.GOOGLE : Source.FACEBOOK).userProfileId(UUID.randomUUID()).build();
     new HashCodeMethodTester().testHashCodeMethod(() -> {
-      return eps;
+      return source.toBuilder().build();
+    });
+
+    new EqualsMethodTester().testEqualsMethod(() -> {
+      return source.toBuilder().build();
     });
   }
 
