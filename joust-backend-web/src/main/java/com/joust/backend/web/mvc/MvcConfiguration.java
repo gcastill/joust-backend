077463c693e8c3f9d5bb822aafe1e866f80b566d
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-19 06:07:33 -0500

    remove some webmvc components

--- joust-backend-web/src/main/java/com/joust/backend/web/mvc/MvcConfiguration.java
+++ joust-backend-web/src/main/java/com/joust/backend/web/mvc/MvcConfiguration.java
@@ -5,47 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
-
 @Configuration
 @ComponentScan(basePackages = "com.joust.backend.web.mvc")
 public class MvcConfiguration extends WebMvcConfigurationSupport {
 
   @Override
-  protected void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/login").setViewName("login");
-  }
-
-  @Override
-  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-  }
-
-  @Bean
-  public ViewResolver getUrlBasedViewResolver() {
-    UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
-    viewResolver.setPrefix("/WEB-INF/jsp/");
-    viewResolver.setSuffix(".jsp");
-    return viewResolver;
-  }
-
-  @Override
   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-      configurer.enable();
+    configurer.enable();
   }
-  
+
   @Bean
   public static PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer() {
     PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
