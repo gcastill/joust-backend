613af493b7513885343631b80794aea60223038e
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-20 07:29:13 -0500

    walking the changes back

--- joust-backend-web/src/main/java/com/joust/backend/web/SecurityConfiguration.java
+++ joust-backend-web/src/main/java/com/joust/backend/web/SecurityConfiguration.java
@@ -1,39 +1,10 @@
 package com.joust.backend.web;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 @ImportResource({ "classpath:beans-security.xml" })
-public abstract class SecurityConfiguration {
-
-  @Autowired
-  private DataSource dataSource;
-
-  @Bean
-  public JdbcClientDetailsService clientDetails() {
-    return new JdbcClientDetailsService(dataSource);
-  }
-
-  @Bean
-  public JdbcTokenStore tokenStore() {
-    return new JdbcTokenStore(dataSource);
-  }
-
-  @Bean
-  public DefaultTokenServices tokenServices() {
-    DefaultTokenServices tokenServices = new DefaultTokenServices();
-    tokenServices.setTokenStore(tokenStore());
-    tokenServices.setSupportRefreshToken(true);
-    tokenServices.setClientDetailsService(clientDetails());
-    return tokenServices;
-  }
+public class SecurityConfiguration {
 
 }
