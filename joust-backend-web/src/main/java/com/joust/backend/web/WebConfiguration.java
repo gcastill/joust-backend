7541c69241d4fca69c441c2b27c08419911a4706
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-20 07:53:17 -0500

    pulling up security

--- joust-backend-web/src/main/java/com/joust/backend/web/WebConfiguration.java
+++ joust-backend-web/src/main/java/com/joust/backend/web/WebConfiguration.java
@@ -2,11 +2,13 @@ package com.joust.backend.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 import com.joust.backend.data.spring.DataConfiguration;
 
 @Configuration
-@Import({ DataConfiguration.class, SecurityConfiguration.class })
+@Import({ DataConfiguration.class })
+@ImportResource({ "classpath:beans-security.xml" })
 public class WebConfiguration {
 
 }
