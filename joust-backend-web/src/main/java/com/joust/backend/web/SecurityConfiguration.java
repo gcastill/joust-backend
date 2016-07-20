d26be77ec14e779e579c457066b1437c221ac68a
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-19 22:59:46 -0500

    moving a little out of beans-security.xml

---
+++ joust-backend-web/src/main/java/com/joust/backend/web/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.joust.backend.web;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@Configuration
+@ImportResource({ "classpath:beans-security.xml" })
+@EnableWebSecurity
+public abstract class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  public abstract DataSource dataSource();
+
+  @Bean
+  public JdbcClientDetailsService clientDetails() {
+    return new JdbcClientDetailsService(dataSource());
+  }
+
+  @Bean
+  public JdbcTokenStore tokenStore() {
+    return new JdbcTokenStore(dataSource());
+  }
+
+}
--- joust-backend-web/src/main/java/com/joust/backend/web/WebConfiguration.java
+++ joust-backend-web/src/main/java/com/joust/backend/web/WebConfiguration.java
@@ -1,10 +1,5 @@
 package com.joust.backend.web;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource({ "classpath:beans-security.xml" })
 public class WebConfiguration {
 
 }
