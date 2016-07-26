ba95c6059e03449c4caa6f9ce1969563225b523a
Gustavo Castillo <twistedmixes@gmail.com>
2016-07-25 23:15:18 -0500

    functional tests are up and running with spring boot.

--- joust-backend-web/src/test/java/com/joust/backend/web/spring/mvc/controller/oauth/google/GoogleControllerFT.java
+++ joust-backend-web/src/test/java/com/joust/backend/web/spring/mvc/controller/oauth/google/GoogleControllerFT.java
@@ -1,7 +1,16 @@
 package com.joust.backend.web.spring.mvc.controller.oauth.google;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.joust.backend.web.spring.Application;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
 public class GoogleControllerFT {
 
   @Test
